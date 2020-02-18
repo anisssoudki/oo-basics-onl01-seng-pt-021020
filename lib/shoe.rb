bc8357d0ac063892ae82e21b0937456c4c9e9246
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-18 23:43:52 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,13 +9,18 @@ class Shoe
     
     @brand = brand
   end
+  
   def cobble=(cobble)
     @cobble = cobble
     puts "#{self} has been repaired"
   end
+  
   def condition=(condition)
     
     @condition = condition
     puts "the #{self}'s condition new"
   end
+  
+  def condition
+    @condition
 end
