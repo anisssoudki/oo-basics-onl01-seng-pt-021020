a14bd638f5acd2f49be209edc4b002ee9876c37a
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-18 23:37:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,40 +1,40 @@
-class Book
+# class Book
   
-  def initialize(title)
-    @title = title
-  end
-  def title
-    @title
-  end
+#   def initialize(title)
+#     @title = title
+#   end
+#   def title
+#     @title
+#   end
   
-  def author=(author_name)
-    @author = author_name
-  end
-  def author
-    @author
-  end
+#   def author=(author_name)
+#     @author = author_name
+#   end
+#   def author
+#     @author
+#   end
   
-  def page_count=(num)
-    @page_count = num
-  end
-  def page_count
-    @page_count
-  end
+#   def page_count=(num)
+#     @page_count = num
+#   end
+#   def page_count
+#     @page_count
+#   end
   
   
   
-  def genre=(book_genre)
-    @genre = book_genre
-  end
-  def genre 
-    @genre
-  end
+#   def genre=(book_genre)
+#     @genre = book_genre
+#   end
+#   def genre 
+#     @genre
+#   end
     
-    def turn_page
-     puts "Flipping the page...wow, you read fast!"
-  end
+#     def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
     
-end
+# end
 
 
 
